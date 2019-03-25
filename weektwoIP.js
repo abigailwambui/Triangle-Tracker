defe2bbf1bb7ab96edb210fb5817185e5c4d8b0b
abigailwambui <abigailw15njuguna@gmail.com>
2019-03-25 09:57:24 +0300

    Add input boxes

--- weektwoIP.js
+++ weektwoIP.js
@@ -1,15 +1,15 @@
-function triangleDetector() {
-var number1 = parseFloat(prompt ("firstNumber"))
-var number2 = parseFloat(prompt ("secondNumber"))
-var number3 = parseFloat(prompt ("thirdNumber"))
+function triangle(){
+var number1 = parseFloat(prompt("firstNumber"))
+var number2 = parseFloat(prompt("secondNumber"))
+var number3 = parseFloat(prompt("thirdNumber"))
 
 var correct = function(number1, number2, number3) {
-	return number1 + number2> number3 && number2 + number3> number1 && number1 + number2> number3
+	return number1 + number2> number3 && number2 + number3> number1 && number1 + number3> number2
   }
   if (correct(number1, number2, number3) && number1===number2 && number2===number3) {
   alert ('Equilateral Triangle');
   }
-  else if (correct(number1, number2, number3) && number1===number2!==number3 && number2===number3!==number1 && number3===number1!==number2) {
+  else if (correct(number1, number2, number3) && number1===number2 || number2===number3 || number3===number1) {
   alert ('Isoceles Triangle');
   }
   else if (correct(number1, number2, number3) &&
