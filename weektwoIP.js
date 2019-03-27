676fae46f8e03e8e2a60bc42e4bb6f7dc28203de
abigailwambui <abigailw15njuguna@gmail.com>
2019-03-27 09:46:00 +0300

    Correct IP

--- weektwoIP.js
+++ weektwoIP.js
@@ -1,22 +1,23 @@
 function triangle(){
-var number1 = parseFloat(prompt("firstNumber"))
-var number2 = parseFloat(prompt("secondNumber"))
-var number3 = parseFloat(prompt("thirdNumber"))
+var number1 = parseFloat(document.getElementById("firstNumber").value);
+var number2 = parseFloat(document.getElementById("secondNumber").value);
+var number3 = parseFloat(document.getElementById("thirdNumber").value);
+var triType = document.getElementById("triType");
 
 var correct = function(number1, number2, number3) {
 	return number1 + number2> number3 && number2 + number3> number1 && number1 + number3> number2
   }
   if (correct(number1, number2, number3) && number1===number2 && number2===number3) {
-  alert ('Equilateral Triangle');
+		triType.textContent = "Equilateral Triangle";
   }
   else if (correct(number1, number2, number3) && number1===number2 || number2===number3 || number3===number1) {
-  alert ('Isoceles Triangle');
+  	triType.textContent = "Isosceles Triangle";
   }
   else if (correct(number1, number2, number3) &&
   number1!==number3 && number2!==number3 && number1!==number2) {
-  alert ('Scalene Triangle');
+  	triType.textContent = "Scalene Triangle";
   }
   else {
-  alert("Not a triangle, try again!")
+  triType.textContent = "Not a triangle, try again!"
 };
 }
